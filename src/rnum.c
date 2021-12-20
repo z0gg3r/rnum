623136d81a83fa6d14ad60272c22a3573dd133f3
zocker <zocker@10zen.eu>
2021-12-20 22:48:18 +0100

    Minimal change

--- src/rnum.c
+++ src/rnum.c
@@ -24,7 +24,11 @@ int main(int argc, char *argv[])
 {
 	time_t t;
 	srand((unsigned) time(&t));
+#ifndef _ALWAYS_RETURN
 	int print = 1;
+#else
+	int print = 0;
+#endif
 	int opt = 0;
 	int range = 0;
 	int range_set = 0;
@@ -60,7 +64,7 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	if (base_set && base == -1 || base > range) {
+	if ((base_set && base == -1) || base > range) {
 		fprintf(stderr, "Please provide a valid base.\n");
 		return EXIT_FAILURE;
 	}
